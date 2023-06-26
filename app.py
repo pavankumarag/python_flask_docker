b85e815a815c475ecef0763c534b1ee5f4767835
pavankumarag <pavankumar.ag@gmail.com>
2023-06-26 15:58:30 +0530

    adding replicate set ability to deployment

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
-
+    return f"<h2>Hello from Flask & Docker from pod: {os.environ.get('HOSTNAME', 'DEFAULT_ENV')} </h2>"
+os
 @app.route('/new')
 def hello_geek_new():
-    return '<h1>Hello from Flask /new endpoint</h2>'
+    return f"<h1>Hello from Flask /new endpoint from pod: {os.environ.get('HOSTNAME', 'DEFAULT_ENV')} </h2>"
 
 
 if __name__ == "__main__":
